f8eff9c8ab828a8d08701f633f191a002e1e89a0
Jojo <53246097+joshoshiro99@users.noreply.github.com>
2021-04-25 13:15:07 -0400

    Created Minimax_data.txt with initial data. Minimax testing code is updated. Algorithm successfully plays itsefl

--- Minimax_testing.py
+++ Minimax_testing.py
@@ -1,81 +1,104 @@
 #!/usr/bin/env python3
-import MiniMaxTicTacToe
+
+# Todo:
+#  -see "TESTING TO FILE OUTPUT"
+#
 
 # testing file for Minimax:
 #   - this file will create an environment that will enable the AI 
 #   to play against itself
 #   - the AI will record the performance of Minimax adding
 #   it to a file with the following fomat:
-#
-#          | First call |   Second   | . . . | Final Call |
-#   -------|------------|------------|-------|------------|
-#   Game 1 | 0.12345678 | 0.12345678 | . . . | 0.12345678 |
-#   -------|------------|------------|-------|------------|
-#   Game 2 | 0.12345678 | 0.12345678 | . . . | 0.12345678 |
-#   -------|------------|------------|-------|------------| 
-#     .    |            |            |       |            |
-#     .    |            |            |       |            |
-#     .    |            |            |       |            |
-#   -------|------------|------------|-------|------------| 
-#   Game N | 0.12345678 | 0.12345678 | . . . | 0.12345678 |
-#   -------|------------|------------|-------|------------| 
+#   Game 0:\tFirst\tSecond\tThird\tFourth\tFifth\tSixth\tSeventh\tEighth\tNinth
+import MiniMaxTicTacToe
 
+#----------AI ALGORITHM----------#
 def comp_turn(a_choice, p_choice, current):
-    #check depth of game tree
+    # retrieve current board
     board = MiniMaxTicTacToe.board
+    
+    # check depth of game tree
     depth = len(MiniMaxTicTacToe.empty_cells(board))
     if depth == 0 or MiniMaxTicTacToe.game_ends(board):
         return
 
-
+    # render
     MiniMaxTicTacToe.clean()
     print(f'Computer turn [{a_choice}]')
     MiniMaxTicTacToe.render(board, a_choice, p_choice)
 
-    # random start
-    
     if depth == 9:
+        # random start
         x = MiniMaxTicTacToe.choice([0, 1, 2])
         y = MiniMaxTicTacToe.choice([0, 1, 2])
-    else: # subsequent turns will be determined with minimax
+    else: 
+        # subsequent turns will be determined with minimax
+        # record initial time
         time_minimax = 0
         minimax_start = MiniMaxTicTacToe.time.time()
 
+        # get the Minimax recommended move to make
         move = MiniMaxTicTacToe.minimax(board, depth, current)
-        x, y = move[0], move[1]
 
+        # output minimax performance
         print('Minimax time:')
         print(MiniMaxTicTacToe.time.time() - minimax_start)
+        
+        # save the move coordinates
+        x, y = move[0], move[1]
 
+    # apply the move to the board
     MiniMaxTicTacToe.make_move(x, y, current)
 
     print()
     MiniMaxTicTacToe.time.sleep(1)
 
+#------------MAIN------------------#
 def main():
     p1_choice = 'X'
     p2_choice = 'O'
 
+    # debug flag
     debug = True
-
-    #gameloop
     board = MiniMaxTicTacToe.board
-    while(not MiniMaxTicTacToe.game_ends(board)):
-         #output to ensure ai can play itself
-        if debug:
-            
+    if debug:
+        #-------DEBUG GAMELOOP--------#
+        while(not MiniMaxTicTacToe.game_ends(board)):
+            # render the board
             MiniMaxTicTacToe.clean()
             MiniMaxTicTacToe.render(board, p1_choice, p2_choice)
 
+            # player 1 (maximizer)
             comp_turn(p1_choice, p2_choice, +1)
 
+            # render results of player 1
             MiniMaxTicTacToe.render(board, p1_choice, p2_choice)
 
+            # player 2 (minimizer)
             comp_turn(p1_choice, p2_choice, -1)
 
+            # render results of player 2
             MiniMaxTicTacToe.render(board, p1_choice, p2_choice)
         else:
+            #-----------TESTING TO FILE OUTPUT-------#
+            # Todo:
+            #  -read in previous game number
+            #  -take in requested number of games
+            #  -game management system
+            #       -start a game
+            #       -record each turns input
+            #       -aggregate data into file append
+            #       -restart game
+            #  
+
+            # for reference, here is the table format:
+            # Game 0:\tFirst\tSecond\tThird\tFourth\tFifth\tSixth\tSeventh\tEighth\tNinth
+
+            # record each turn's performance, add to performance array
+            # output performance in above format
+            # maximizer
             comp_turn(board, p1_choice, p2_choice)
+            # minimizer
             comp_turn(board, p2_choice, p1_choice)
 
 if __name__ == "__main__":
